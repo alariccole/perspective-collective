01f86b632f31fd3a8f3c4efc311166062e1605fb
alariccole <alaric@waveui.com>
2013-09-05 19:59:41 -0700

    Initial Commit

---
+++ PerspectiveCollective/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PerspectiveCollective
+//
+//  Created by Alaric Cole on 9/5/13.
+//  Copyright (c) 2013 Alaric Cole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
